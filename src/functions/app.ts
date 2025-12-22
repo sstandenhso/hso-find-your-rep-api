60d4411c02f4574be5482a50f80edd5f8de6b83e
sstandenhso <sean.standen@henryschein.com>
2025-12-22 11:50:06 -0800

    Re-added old routes.

--- src/functions/app.ts
+++ src/functions/app.ts
@@ -8,21 +8,21 @@ import { searchByStateAPI } from '../api/read/searchByStateAPI';
 import { getRepsAPI } from '../api/read/getRepsAPI';
 
 console.log("DEBUG: app.ts - Starting function registration.");
-// app.http('salesRepApi', {
-//     methods: ['GET', 'POST'], // Keep methods as you need them
-//     authLevel: 'anonymous',
-//     handler: salesRepApi,
-//     route: 'sales-rep' // <-- ADDED THIS LINE
-// });
-// console.log("DEBUG: app.ts - salesRepApi registered.");
+app.http('salesRepApi', {
+    methods: ['GET', 'POST'], // Keep methods as you need them
+    authLevel: 'anonymous',
+    handler: salesRepApi,
+    route: 'sales-rep' // <-- ADDED THIS LINE
+});
+console.log("DEBUG: app.ts - salesRepApi registered.");
 
-// app.http('searchApi', {
-//     methods: ['GET', 'POST'], // Keep methods as you need them
-//     authLevel: 'anonymous',
-//     handler: searchApi,
-//     route: 'search' // <-- ADDED THIS LINE
-// });
-// console.log("DEBUG: app.ts - searchApi registered.");
+app.http('searchApi', {
+    methods: ['GET', 'POST'], // Keep methods as you need them
+    authLevel: 'anonymous',
+    handler: searchApi,
+    route: 'search' // <-- ADDED THIS LINE
+});
+console.log("DEBUG: app.ts - searchApi registered.");
 
 app.http('getRepsAPI', {
     methods: ['GET', 'POST'], // Keep methods as you need them
